a506e7ab4dd7b584b44afbb2701e4a527488e55e
André Filipe de Assunção e Brito <decko@ufpr.br>
2012-10-23 14:26:15 -0200

    Pequenos erros.

--- conselhos/settings.py
+++ conselhos/settings.py
@@ -55,7 +55,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(CURRENT_DIR, 'static'),
+    os.path.join(CURRENT_DIR, '../static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -93,7 +93,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(CURRENT_DIR, 'tmpl'),
+    os.path.join(CURRENT_DIR, '../tmpl'),
 )
 
 # A sample logging configuration. The only tangible logging
